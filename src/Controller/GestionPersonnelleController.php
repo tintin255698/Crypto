4fccdabff15a752eb671aa10615a53aaa17a2507
tintin <vivien.joly@hotmail.fr>
2020-07-25 12:57:51 +0200

    MAJ

--- src/Controller/GestionPersonnelleController.php
+++ src/Controller/GestionPersonnelleController.php
@@ -5,17 +5,20 @@ namespace App\Controller;
 
 
 use App\Entity\Announces;
+use App\Entity\MotDePasse;
 use App\Entity\User;
 use App\Form\EditUserType;
 use App\Form\GestionPersonnelleType;
+use App\Form\MdpType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class GestionPersonnelleController extends AbstractController
 {
     /**
-     * @Route("/gestion/personnelle/", name="gestion_personnelle_modification")
+     * @Route("/gestion/personnelle", name="gestion_personnelle_modification")
      */
 
     public function modification(Request $request)
@@ -47,6 +50,52 @@ class GestionPersonnelleController extends AbstractController
             'user' => $form->createView()
         ]);
     }
+
+    /**
+     * @Route("/gestion/motdepasse", name="gestion_mdp_modification")
+     */
+
+    public function motDePasse(Request $request, UserPasswordEncoderInterface $encoder)
+    {
+        $maj = new MotDePasse();
+
+        $user = $this->getUser();
+
+        $form = $this->createForm(MdpType::class, $maj);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            if(!password_verify($maj->getAncienMdp(), $user->getPassword())){
+                //gerer l'erreur
+            } else {
+                $nouveau = $maj->getNouveauMdp();
+                $password = $encoder->encodePassword($user, $nouveau);
+
+                $user->setPassword($password);
+
+                $doctrine = $this->getDoctrine()->getManager();
+                $doctrine->persist($user);
+                $doctrine->flush();
+            }
+
+            $this->addFlash(
+                'success',
+                '<strong>Votre mot de passe a bien ete modifie !</strong>'
+            );
+
+            return $this->redirectToRoute('home');
+
+
+
+        }
+
+        return $this->render('gestion_personnelle/mdp.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+
+
 
 }
 
